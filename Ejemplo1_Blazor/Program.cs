54c36ca4b9a529a11755870cdfe1b1d98d740cd0
Carlojavlop <jloepez2@gmail.co>
2022-12-02 14:56:25 -0600

    comint

--- Ejemplo1_Blazor/Program.cs
+++ Ejemplo1_Blazor/Program.cs
@@ -1,6 +1,21 @@
 using Ejemplo1_Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+
+//public static async Task Main(string[] args)
+//{
+//    var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//    builder.RootComponents.Add<App>("app");
+
+//    builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//    builder.Services.AddScoped<DialogService>();
+//    builder.Services.AddScoped<NotificationService>();
+//    builder.Services.AddScoped<TooltipService>();
+//    builder.Services.AddScoped<ContextMenuService>();
+//    await builder.Build().RunAsync();
+//}
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,10 +23,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+
 builder.Services.AddOidcAuthentication(options =>
 {
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
     builder.Configuration.Bind("Local", options.ProviderOptions);
 });
 
