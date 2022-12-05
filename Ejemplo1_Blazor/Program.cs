cfce9ddc1622673de0055417d81573eb34277441
Carlojavlope <93419901+Carlojavlope@users.noreply.github.com>
2022-12-04 21:54:22 -0600

    ComintPERFIL

--- Ejemplo1_Blazor/Program.cs
+++ Ejemplo1_Blazor/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
 
 
+
 //public static async Task Main(string[] args)
 //{
 //    var builder = WebAssemblyHostBuilder.CreateDefault(args);
